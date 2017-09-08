0ef6f1aed72e22ac45f78b40dadf8b7d052eb86d
PeachTreeOath <peachtreeoath@gmail.com>
2017-09-08 11:48:29 -0600

    Changed long run behavior to farm

--- src/ChampRunner.java
+++ src/ChampRunner.java
@@ -5,13 +5,12 @@ import java.awt.Point;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
 
 public class ChampRunner {
 	private boolean longRun = false;
 	private static final double cutOffDelay = 1000 * 60 * 60;
 	private static final double cutOffHour = 0.5;
-	private static final double cutOffOverride = 0.5;
+	private static final int longRunPressDelay = 1000 * 60 * 5;
 	private final int startDelay = 3000;
 	private final int clickDelay = 500;
 	private final int stopDistance = 50;
@@ -25,6 +24,7 @@ public class ChampRunner {
 	long startTime;
 	double cutOffTime;
 	boolean resetGame;
+	long longRunFarmTime;
 	int resetNum = 1;
 	int skillXIncrement = 15;
 	int perkXIncrement = 25;
@@ -35,7 +35,7 @@ public class ChampRunner {
 	private Point chooseMissionPoint;
 	private Point confirmMissionPoint;
 	private Point autoProgressPoint;
-	
+
 	public static void main(String[] args) {
 		try {
 			robot = new Robot();
@@ -57,11 +57,8 @@ public class ChampRunner {
 			resetGame = false;
 			startTime = System.currentTimeMillis();
 
-			if (resetNum == 1 && !longRun) {
-				cutOffTime = System.currentTimeMillis() + cutOffDelay * cutOffOverride;
-			} else {
-				cutOffTime = System.currentTimeMillis() + cutOffDelay * cutOffHour;
-			}
+			cutOffTime = System.currentTimeMillis() + cutOffDelay * cutOffHour;
+
 			Thread.sleep(startDelay);
 			System.out.println("Full Bot started");
 
@@ -74,21 +71,32 @@ public class ChampRunner {
 			Point lastPoint = MouseInfo.getPointerInfo().getLocation();
 			Toolkit.getDefaultToolkit().beep();
 
-			outerloop:
-			while (true) {
+			outerloop: while (true) {
 				Point currentPoint = MouseInfo.getPointerInfo().getLocation();
 				robot.mousePress(InputEvent.BUTTON1_MASK);
-				Thread.sleep(10);
+				Thread.sleep(30);
 				robot.mouseRelease(InputEvent.BUTTON1_MASK);
 
-				if(ArrowExists())
-				{
-					resetGame = true;
-					resetNum++;
-					resetGame();
-					break;
+				if (ArrowExists() && System.currentTimeMillis() > longRunFarmTime) {
+					// Farm for x seconds then check for arrow again if doing long run
+					if(longRun)
+					{
+						// Turn on auto-progress
+						robot.mouseMove(autoProgressPoint.x, autoProgressPoint.y);
+						Thread.sleep(50);
+						pressMouse(1000);
+						
+						longRunFarmTime =  System.currentTimeMillis() + longRunPressDelay;
+					}
+					else
+					{
+						resetGame = true;
+						resetNum++;
+						resetGame();
+						break;
+					}
 				}
-				
+
 				if (Math.abs(lastPoint.x - currentPoint.x) > stopDistance
 						|| Math.abs(lastPoint.y - currentPoint.y) > stopDistance) {
 					break;
@@ -102,29 +110,27 @@ public class ChampRunner {
 					int currx = buffStartingPoint.x;
 					robot.mouseMove(currx, buffStartingPoint.y);
 					lastPoint = MouseInfo.getPointerInfo().getLocation();
-					for(int i = 0; i < 60; i++)
-					{
+					for (int i = 0; i < 60; i++) {
 						currentPoint = MouseInfo.getPointerInfo().getLocation();
 						robot.mousePress(InputEvent.BUTTON1_MASK);
 						Thread.sleep(30);
 						robot.mouseRelease(InputEvent.BUTTON1_MASK);
 
-						if(ArrowExists())
-						{
+						if (ArrowExists()) {
 							resetGame = true;
 							resetNum++;
 							resetGame();
 							break outerloop;
 						}
-						
+
 						if (Math.abs(lastPoint.x - currentPoint.x) > stopDistance
 								|| Math.abs(lastPoint.y - currentPoint.y) > stopDistance) {
 							break outerloop;
 						}
-						
+
 						currx -= skillXIncrement;
 						robot.mouseMove(currx, buffStartingPoint.y);
-						
+
 						lastPoint = MouseInfo.getPointerInfo().getLocation();
 						Thread.sleep(150);
 					}
@@ -133,35 +139,33 @@ public class ChampRunner {
 					int currx2 = perkStartingPoint.x;
 					robot.mouseMove(currx2, perkStartingPoint.y);
 					lastPoint = MouseInfo.getPointerInfo().getLocation();
-					for(int i = 0; i < 32; i++)
-					{
+					for (int i = 0; i < 32; i++) {
 						currentPoint = MouseInfo.getPointerInfo().getLocation();
 						robot.mousePress(InputEvent.BUTTON1_MASK);
 						Thread.sleep(30);
 						robot.mouseRelease(InputEvent.BUTTON1_MASK);
 
-						if(ArrowExists())
-						{
+						if (ArrowExists()) {
 							resetGame = true;
 							resetNum++;
 							resetGame();
 							break outerloop;
 						}
-						
+
 						if (Math.abs(lastPoint.x - currentPoint.x) > stopDistance
 								|| Math.abs(lastPoint.y - currentPoint.y) > stopDistance) {
 							break outerloop;
 						}
-						
+
 						currx2 += perkXIncrement;
 						robot.mouseMove(currx2, perkStartingPoint.y);
-						
+
 						lastPoint = MouseInfo.getPointerInfo().getLocation();
 						Thread.sleep(150);
 					}
 					break;
 				}
-				
+
 				step++;
 				if (step == 3)
 					step = 0;
@@ -179,7 +183,7 @@ public class ChampRunner {
 		buffStartingPoint = new Point(originPoint.x - (int) (0 * scale), originPoint.y + (int) (57 * scale));
 		perkStartingPoint = new Point(originPoint.x - (int) (756 * scale), originPoint.y - (int) (65 * scale));
 		goPoint = new Point(originPoint.x + (int) (40 * scale), originPoint.y - (int) (450 * scale));
-		
+
 		completeAdventurePoint = new Point(originPoint.x - (int) (971 * scale), originPoint.y - (int) (556 * scale));
 		confirmCompletePoint = new Point(originPoint.x - (int) (616 * scale), originPoint.y - (int) (115 * scale));
 		continuePoint = new Point(originPoint.x - (int) (532 * scale), originPoint.y - (int) (39 * scale));
@@ -231,8 +235,8 @@ public class ChampRunner {
 			// Confirm mission
 			robot.mouseMove(confirmMissionPoint.x, confirmMissionPoint.y);
 			Thread.sleep(50);
-			pressMouse(2000);
-			
+			pressMouse(2500);
+
 			// Turn on auto-progress
 			robot.mouseMove(autoProgressPoint.x, autoProgressPoint.y);
 			Thread.sleep(50);
@@ -241,7 +245,7 @@ public class ChampRunner {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private int numArrowHits = 0;
 
 	private boolean ArrowExists() {
@@ -254,13 +258,11 @@ public class ChampRunner {
 		}
 		if (numArrowHits > 8) {
 			numArrowHits = 0;
-			if(!longRun)
-				return true;
 		}
 
 		return false;
 	}
-	
+
 	private boolean TestColorForArrow(Color color) {
 		if (color.getRed() < 220) {
 			return false;
